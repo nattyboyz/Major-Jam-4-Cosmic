dabc8bf3ac9b30716ed7dc12026db4fe4e35682f
NattyboyZ <iamnattyboyz@hotmail.com>
2021-08-14 15:50:03 +0700

    WIP add buy UI Saperate Rating UI abnd Money UI and customer left UI

---
+++ Assets/BuyUI.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyUI : BaseUI
+{
+    
+    public void Btn_Done()
+    {
+        Hide();
+    }
+
+}
--- Assets/ShopOpenUI.cs
+++ Assets/ShopOpenUI.cs
@@ -5,15 +5,11 @@ using System;
 using DG.Tweening;
 using UnityEngine.UI;
 
-public class ShopOpenUI : MonoBehaviour
+public class ShopOpenUI : BaseUI
 {
+    [SerializeField] BuyUI buyUi;
     [SerializeField] Button open_btn;
     [SerializeField] Button close_btn;
-
-    [Header("Animation")]
-    [SerializeField] Animator animator;
-    [SerializeField] AnimationClip in_clip;
-    [SerializeField] AnimationClip out_clip;
 
     public Action onOpen;
     public Action onClose;
@@ -35,22 +31,28 @@ public class ShopOpenUI : MonoBehaviour
 
     IEnumerator ieOpen()
     {
-        animator.SetTrigger("in");
-        yield return new WaitForSeconds(in_clip.length);
+        //animator.SetTrigger("in");
+        //yield return new WaitForSeconds(in_clip.length);
+        yield return ieShow();
         onOpen?.Invoke();
     }
-
 
     public void Btn_Close()
     {
         StartCoroutine(ieClose());
     }
 
+    public void Btn_Buy()
+    {
+        buyUi.Show();
+    }
+
 
     IEnumerator ieClose()
     {
-        animator.SetTrigger("out");
-        yield return new WaitForSeconds(out_clip.length);
+        //animator.SetTrigger("out");
+        //yield return new WaitForSeconds(out_clip.length);
+        yield return ieHide();
         onClose?.Invoke();
     }
 }
